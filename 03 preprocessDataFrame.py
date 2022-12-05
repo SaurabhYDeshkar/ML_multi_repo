af539865d9a45f6f1b8463dea6ca0d070bbd4901
SaurabhYDeshkar <116559570+SaurabhYDeshkar@users.noreply.github.com>
2022-12-05 16:51:47 +0530

    Update 03 preprocessDataFrame.py

--- 03 preprocessDataFrame.py
+++ 03 preprocessDataFrame.py
@@ -1,13 +1,20 @@
 var_df= pd.read_csv(path="", index_col=0) ##The first Column can be Index or irrelevant, change it to INDEX_COL
+
 ##Setting Dummies if OUTPUT Data has incompatible values, convert to Binary when Classifying
 dum_vardf= pd.get_dummies(var_df, drop_first=True)
+
 ## Dropping unnecessary Columns from the X Dataset
 X = dum_vardf.drop('output',axis=1) ##Dropping Column for Prediction Value
 y = dum_vardf['output'] ##Making the Predictions column as a Column Matrix
 
+##Label Encoding the Output if needed
+labenc= LabelEncoder()
+labenc_y = labenc.fit_transform(y)
+
 ##If X is needed to be reshaped from a Row Vector to Column Vector
 X = X.values
 X = X.reshape(-1,1)
+
 ##Using Train-Test Split
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state= 108, 
                                                     test_size= 0.27, 
