0732f69570b9c3f10e905c2bc781721f9ae2d344
SaurabhYDeshkar <116559570+SaurabhYDeshkar@users.noreply.github.com>
2022-12-05 15:24:06 +0530

    Create ensemblingAlgorithms.py

---
+++ ensemblingAlgorithms.py
@@ -0,0 +1,12 @@
+###########################
+###VOTING####
+from sklearn.ensemble import VotingClassifier, VotingRegressor
+###BAGGING####
+from sklearn.ensemble import BaggingClassifier, BaggingRegressor
+###BOOSTING####
+##GBM--- Slow
+from sklearn.ensemble import GradientBoostingClassifier, GradientBoostingRegressor
+##XGB--- Faster operation, requires different library
+from xgboost import XGBClassifier, XGBRegressor
+###STACKING####
+from sklearn.ensemble import StackingClassifier, StackingRegressor
