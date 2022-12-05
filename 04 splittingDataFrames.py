702645868aa34d8cc43fe00d864fa7b0bff93555
SaurabhYDeshkar <116559570+SaurabhYDeshkar@users.noreply.github.com>
2022-12-05 15:56:46 +0530

    Update 04 splittingDataFrames.py

--- 04 splittingDataFrames.py
+++ 04 splittingDataFrames.py
@@ -1,1 +1,1 @@
-X_train, X_test, y_train, y_test = train_test_split(X, y,random_state=108, test_size=0.27)
+X_train, X_test, y_train, y_test = train_test_split(X, y,random_state=108, test_size=0.27, stratify=y, )
